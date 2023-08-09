9851a85a70b9c281c90891e3d7c08ad16ca2bd8a
Tushar Srivastava <tushar812srivastava@gmail.com>
2023-08-09 09:33:43 +0530

    okkk

---
+++ src/components/History.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+const History = ({ history, moveTo, currentMove }) => {
+    return (
+      <div className="history-wrapper">
+        <ul className="history">
+          {history.map((_, index) => (
+            <li key={index}>
+              <button
+                type="button"
+                className={`btn-move ${currentMove === index ? 'active' : ''}`}
+                onClick={() => moveTo(index)}
+              >
+                {index === 0 ? 'Go to game start' : `Go to move #${index}`}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default History;
---
+++ src/components/Square.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const Square = ({ value, onClick, isWinningSquare }) => {
+    const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+    const winningClassName = isWinningSquare ? 'winning' : '';
+  
+    return (
+      <button
+        type="button"
+        className={`square ${colorClassName} ${winningClassName}`}
+        onClick={onClick}
+      >
+        {value}
+      </button>
+    );
+  };
+  
+  export default Square;
